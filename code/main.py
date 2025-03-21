c13b0ab545a7b5333b318870429234acac23be94
miloswrath <miloswrath@users.noreply.github.com>
2025-03-21 09:27:28 -0500

    added correct paths for vosslab linux

--- code/main.py
+++ code/main.py
@@ -2,9 +2,9 @@ from utils.mnt import create_symlinks
 from utils.save import Save
 from core.gg import GG
 
-INT_DIR = '/Volumes/vosslabhpc/Projects/BOOST/InterventionStudy/3-experiment/data/act-int-test'
-OBS_DIR = '/Volumes/vosslabhpc/Projects/BOOST/ObservationalStudy/3-experiment/data/act-obs-test'
-RDSS_DIR = '/Volumes/VossLab/Repositories/Accelerometer_Data'
+INT_DIR = '/mnt/nfs/lss/vosslabhpc/Projects/BOOST/InterventionStudy/3-experiment/data/act-int-test'
+OBS_DIR = '/mnt/nfs/lss/vosslabhpc/Projects/BOOST/ObservationalStudy/3-experiment/data/act-obs-test'
+RDSS_DIR = '/mnt/nfs/rdss/vosslab/Repositories/Accelerometer_Data'
 
 class pipe:
     def __init__(self):
