c88cb495ac5080819dc21d835afb4b12dcdf554e
miloswrath <miloswrath@users.noreply.github.com>
2025-08-01 08:44:39 -0500

    automated commit by vosslab linux

--- code/utils/save.py
+++ code/utils/save.py
@@ -8,6 +8,7 @@ class Save:
     def __init__(self, intdir, obsdir, rdssdir, token, daysago=None):
         results = ID_COMPARISONS('../mnt', token, daysago).compare_ids()
         self.matches = results['matches']
+        print(self.matches)
         self.dupes = results['duplicates']
         print(f"Type of Dupes: {type(self.dupes)}")
         self.INT_DIR = intdir
