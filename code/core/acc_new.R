fc74cc5a6651cc42d32dd240a529bc077acfe0ed
miloswrath <miloswrath@users.noreply.github.com>
2025-08-15 01:18:40 -0500

    automated commit by vosslab linux

--- code/core/acc_new.R
+++ code/core/acc_new.R
@@ -111,7 +111,7 @@ main <- function() {
           windowsizes = c(5, 900, 3600),
 
           # ==== Part 2: Non-wear detection ====
-          ignorenonwear = FALSE,
+          ignorenonwear = TRUE,
 
           # ==== Part 3: Sleep detection ====
            loglocation = SleepLog,
